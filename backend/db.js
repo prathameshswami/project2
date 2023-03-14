43e94ca3cd040e6812db3f7933c4261ac358b011
prathameshswami <103546660+prathameshswami@users.noreply.github.com>
2023-03-14 19:23:42 +0530

    Add files via upload

---
+++ backend/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb+srv://gofood:gofood@cluster0.tyaik9r.mongodb.net/gofoodmern?retryWrites=true&w=majority';
+
+const mongoDB = async () => {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
+        if (err) console.log("---", err);
+        else {
+            console.log("Connected");
+            const fetched_data = await mongoose.connection.db.collection("food_items");
+            fetched_data.find({}).toArray(async (err, data) => {
+                const foodCategory = await mongoose.connection.db.collection("foodCategory");
+                foodCategory.find({}).toArray(async (err, catData) => {
+                    if (err) console.log(err);
+                    else {
+                        global.food_items = data;
+                        global.foodCategory = catData;
+                    }
+                })
+            })
+        }
+    })
+}
+
+module.exports = mongoDB;
+
